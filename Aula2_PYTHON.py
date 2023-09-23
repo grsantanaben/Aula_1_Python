3f6c218fc416ad8e7ea0cda9edab87bf1676bf71
grsantanaben <145858679+grsantanaben@users.noreply.github.com>
2023-09-23 17:18:07 -0300

    Add files via upload

---
+++ Aula2_PYTHON.py
@@ -0,0 +1,53 @@
+                                                           #python é uma linguagem: 
+# dinamica / interpretada/ OOP / alto nivel 
+# convensões  ==  boas práticas
+# nomear de forma semântica  == dar o nome daquilo que o objeto faz
+# utilizar snake case
+# inglês  =  o código precisa ser universal 
+# crie um sistema que mostre um texto na tela
+# int - numeros inteiros
+# floar - numeros quebradas ex:1,5
+# string
+
+# == igualdade 
+# = atribuição
+# < menor que 
+# > maior que
+# != diferente
+# <= menor ou igual
+# >= maior ou igual 
+
+# numero = 2
+# numero2 = 10
+# numero3 = numero
+
+
+# nome = input ("Qual o seu nome\n ?")
+# print ("Ola, tudo bem !",nome)
+
+# print ("Calculadora de soma")
+# n1 = float(input ("Digite um numero ->\n"))
+# n2 = float(input ("Digite o numero 2->\n"))
+# n3 = n1 + n2
+# print ("O resultado da some é:",n3)
+
+############################## Atividades ######################################
+#1
+n1 = int(input ("Digite o primeiro numero ->\n"))
+n2 = int(input ("Digite o segundo numero ->\n"))
+n3 = n1 + n2
+print ("O resultado da soma é: ->",n3)
+
+#2
+nomeatv = "Gabriel,"
+print (nomeatv)
+
+#3
+Cumprimentoatv = "Seja bem vindo."
+print (nomeatv, Cumprimentoatv)
+
+#4
+nm1= float(input ("Digite o primeiro numero ->\n"))
+nm2 = float(input ("Digite o segundo numero ->\n"))
+nm3 = nm1 / nm2
+print ("O resultado da soma é: ->",nm3)
